021971fd76596ced6172162045ce1850e145d61d
m.kishi <m.kishi.5@gmail.com>
2023-10-07 23:13:42 +0900

    App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,23 @@
+<!-- ==================================================
+// ©2023 https://github.com/m-kishi
+// ================================================ -->
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="light-blue">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Mylish</a>
+    </div>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial、Helvetica Neue、Helvetica、sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.nav-wrapper {
+  margin-left: 1.0rem;
 }
 </style>
